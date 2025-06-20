6c16c18fd682fea1db845ab8c1e3af862f05cc79
jack <52892975+jackkroll@users.noreply.github.com>
2025-06-20 12:05:59 -0400

    Mid season patch update

--- Sources/MapTimerServer/routes.swift
+++ Sources/MapTimerServer/routes.swift
@@ -6,6 +6,11 @@ func routes(_ app: Application) throws {
     let mapSchedulePubs = MapSchedule(origin: originPubs, rotation: [.OL,.SP,.WE])
     let originRanked = Map(name: .OL, availableAt: 1743094800, availableTo: 1743181200)
     let mapScheduleRanked = MapSchedule(origin: originRanked, rotation: [.OL,.SP,.WE])
+    
+    let originPubsMid = Map(name: .BM, availableAt: 1750692600, availableTo: 1750784400)
+    let mapSchedulePubsMid = MapSchedule(origin: originPubsMid, rotation: [.KC,.WE,.BM])
+    let originRankedMid = Map(name: .BM, availableAt: 1750698000, availableTo: 1750784400)
+    let mapScheduleRankedMid = MapSchedule(origin: originRankedMid, rotation: [.KC,.WE,.BM])
     /*
     let epgExtreme = MapSchedule(origin: originPubs, rotation: [.KC,.SP,.ED], takeoverName: "EPG Extreme", takeoverSystemImage: "exclamationmark.shield.fill")
     let straightShotQuads = MapSchedule(origin: originPubs, rotation: [.KC,.SP,.ED], takeoverName: "Straight Shot Quads", takeoverSystemImage: "clock.arrow.trianglehead.2.counterclockwise.rotate.90")
@@ -16,7 +21,16 @@ func routes(_ app: Application) throws {
         ])
     */
     
-    //add season start/end date protections
+    let pubsModeSchedule = ModeSchedule(schedules: [
+        PublishableSchedule(schedule: mapSchedulePubs, usable: DateInterval(start: .distantPast, end: Date(timeIntervalSince1970: 1750784400))),
+        PublishableSchedule(schedule: mapSchedulePubsMid, usable: DateInterval(start: Date(timeIntervalSince1970:1750784400), end: .distantFuture))
+    ])
+    
+    let rankedModeSchedule = ModeSchedule(schedules: [
+        PublishableSchedule(schedule: mapScheduleRanked, usable: DateInterval(start: .distantPast, end: Date(timeIntervalSince1970: 1750784400))),
+        PublishableSchedule(schedule: mapScheduleRankedMid, usable: DateInterval(start: Date(timeIntervalSince1970:1750784400), end: .distantFuture))
+    ])
+    
     
     app.get { req async throws in
         try await req.view.render("index", ["title": "Hello Vapor!"])
@@ -30,23 +44,24 @@ func routes(_ app: Application) throws {
     }
     
     app.get("pubs", "schedule") { req -> MapSchedule in
-        return mapSchedulePubs
+        return pubsModeSchedule.currentSchedule(at: .now) ?? mapSchedulePubsMid
     }
     app.get("pubs","current") { req -> [Map] in
-        return mapSchedulePubs.upcomingMaps(at: .now, range: 0...3)
+        return pubsModeSchedule.currentSchedule(at: .now)?.upcomingMaps(at: .now, range: 0...3) ?? mapSchedulePubsMid.upcomingMaps(at: .now, range: 0...3)
     }
     
     app.get("hash") { req async -> String in
         //add ltm to hash later :)
-        let hashableContent : String = originPubs.availableAt.description + originRanked.availableAt.description
+        //let hashableContent : String = originPubs.availableAt.description + originRanked.availableAt.description
+        let hashableContent : String = pubsModeSchedule.hashString() + rankedModeSchedule.hashString()
         return SHA256.hash(data: Data(hashableContent.utf8)).hex
     }
     
     app.get("ranked","schedule") { req -> MapSchedule in
-        return mapScheduleRanked
+        return rankedModeSchedule.currentSchedule(at: .now) ?? mapScheduleRankedMid
     }
     app.get("ranked","current") { req -> [Map] in
-        return mapScheduleRanked.upcomingMaps(at: .now, range: 0...3)
+        return rankedModeSchedule.currentSchedule(at: .now)?.upcomingMaps(at: .now, range: 0...3) ?? mapScheduleRankedMid.upcomingMaps(at: .now, range: 0...3)
     }
     app.get("ltm", "schedule") { req -> [MapSchedule] in
         var schedules : [MapSchedule] = []
